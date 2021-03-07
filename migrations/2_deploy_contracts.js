857719792c8a7cadc38fb5f5efca99a82f8361c7
korrio <korr@aq1.co>
2021-03-07 14:59:14 +0700

    init CNX sushiswap

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,60 @@
+const Factory = artifacts.require('uniswapv2/UniswapV2Factory.sol');
+const Router = artifacts.require('uniswapv2/UniswapV2Router02.sol');
+const WETH = artifacts.require('WETH.sol');
+const MockERC20 = artifacts.require('MockERC20.sol');
+const PhoenixToken = artifacts.require('PhoenixToken.sol') 
+const MasterChef = artifacts.require('MasterChef.sol'); 
+const SushiBar = artifacts.require('SushiBar.sol');
+const SushiMaker = artifacts.require('SushiMaker.sol');
+const Migrator = artifacts.require('Migrator.sol');
+
+module.exports = async function(deployer, _network, addresses) {
+  const [admin, _] = addresses;
+
+  await deployer.deploy(WETH);
+  const weth = await WETH.deployed();
+  const tokenA = await MockERC20.new('Token A', 'TKA', web3.utils.toWei('1000'));
+  const tokenB = await MockERC20.new('Token B', 'TKB', web3.utils.toWei('1000'));
+
+  await deployer.deploy(Factory, admin);
+  const factory = await Factory.deployed();
+  await factory.createPair(weth.address, tokenA.address);
+  await factory.createPair(weth.address, tokenB.address);
+  await deployer.deploy(Router, factory.address, weth.address);
+  const router = await Router.deployed();
+
+  await deployer.deploy(PhoenixToken);
+  const sushiToken = await PhoenixToken.deployed();
+
+  await deployer.deploy(
+    MasterChef,
+    sushiToken.address,
+    admin,
+    web3.utils.toWei('100'),
+    1,
+    10
+  );
+  const masterChef = await MasterChef.deployed();
+  await sushiToken.transferOwnership(masterChef.address);
+
+  await deployer.deploy(SushiBar, sushiToken.address);
+  const sushiBar = await SushiBar.deployed();
+
+  await deployer.deploy(
+    SushiMaker,
+    factory.address, 
+    sushiBar.address, 
+    sushiToken.address, 
+    weth.address
+  );
+  // const sushiMaker = await SushiMaker.deployed();
+  // await factory.setFeeTo(sushiMaker.address);
+
+  await deployer.deploy(
+    Migrator,
+    masterChef.address,
+    '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
+    factory.address,
+    1
+  );
+};
---
+++ truffle-config.js
@@ -0,0 +1,72 @@
+const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
+const HDWalletProvider_ = require("truffle-hdwallet-provider");
+const privateKey = "";
+const endpointUrl = "https://kovan.infura.io/v3/82aa14abbb9947de867e24a5f4cf8c0c";
+
+const privateKeyBsc = "";
+const mnemonic = "";
+const bscProvider = new HDWalletProvider_(mnemonic, 'https://data-seed-prebsc-1-s1.binance.org:8545');
+
+module.exports = {
+  networks: {
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(
+          //private keys array
+          [privateKey],
+          //url to ethereum node
+          endpointUrl
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 42
+    },
+    bsc: {
+      provider: () => bscProvider,
+      network_id: 97,
+      gas: 5000000,
+      gasPrice: 25000000000,
+    },
+    development: {
+      host: "127.0.0.1",
+      port: 9545,
+      network_id: "5777",
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.6.12"
+    }
+  },
+  plugins: [
+    'truffle-contract-size'
+  ]
+}
+
+// module.exports = {
+//   // Uncommenting the defaults below
+//   // provides for an easier quick-start with Ganache.
+//   // You can also follow this format for other networks;
+//   // see <http://truffleframework.com/docs/advanced/configuration>
+//   // for more details on how to specify configuration options!
+//   //
+//   //networks: {
+//   //  development: {
+//   //    host: "127.0.0.1",
+//   //    port: 7545,
+//   //    network_id: "*"
+//   //  },
+//   //  test: {
+//   //    host: "127.0.0.1",
+//   //    port: 7545,
+//   //    network_id: "*"
+//   //  }
+//   //}
+//   //
+//   compilers: {
+//     solc: {
+//       version: "0.6.12"
+//     }
+//   }
+// };
