9b29f6c7b863fa078ba0b0be33a1ac5a707ac8b6
korrio <korr@aq1.co>
2021-04-03 13:12:18 +0700

    refine for PSU

--- truffle-config.js
+++ truffle-config.js
@@ -1,23 +1,24 @@
 const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
 const HDWalletProvider_ = require("truffle-hdwallet-provider");
 const privateKey = "";
+const privateKeyBsc = "";
+
 const endpointUrl = "https://kovan.infura.io/v3/82aa14abbb9947de867e24a5f4cf8c0c";
 
-const privateKeyBsc = "";
+const kovanProvider = new HDWalletProvider(
+  //private keys array
+  [privateKey],
+  //url to ethereum node
+  endpointUrl
+);
+
 const mnemonic = "";
 const bscProvider = new HDWalletProvider_(mnemonic, 'https://data-seed-prebsc-1-s1.binance.org:8545');
 
 module.exports = {
   networks: {
     kovan: {
-      provider: function() {
-        return new HDWalletProvider(
-          //private keys array
-          [privateKey],
-          //url to ethereum node
-          endpointUrl
-        )
-      },
+      provider: () => kovanProvider,
       gas: 5000000,
       gasPrice: 25000000000,
       network_id: 42
@@ -30,8 +31,8 @@ module.exports = {
     },
     development: {
       host: "127.0.0.1",
-      port: 9545,
-      network_id: "5777",
+      port: 8545,
+      network_id: "*",
     }
   },
   compilers: {
